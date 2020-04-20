cf080a684073cb388da09a1457b3775dd01258ce
S3CORP\toan.nguyenx <toannx95@gmail.com>
2020-04-20 15:06:25 +0700

    update

--- src/config/InitDB.go
+++ src/config/InitDB.go
@@ -3,7 +3,6 @@ package config
 import (
 	"database/sql"
 	"fmt"
-
 	//"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	//"github.com/joho/godotenv"
@@ -15,9 +14,10 @@ func OpenDB() (*sql.DB, error) {
 	//if env != nil {
 	//	fmt.Print(env)
 	//}
-	//
+
 	//username := os.Getenv("DB_USER")
 	//password := os.Getenv("DB_PASSWORD")
+	//dbType := os.Getenv("DB_TYPE")
 	//dbName := os.Getenv("DB_NAME")
 	//dbHost := os.Getenv("DB_HOST")
 	//dbPort := os.Getenv("DB_PORT")
@@ -30,8 +30,8 @@ func OpenDB() (*sql.DB, error) {
 	dbHost := "mydb"
 	dbPort := "3306"
 
-	conn, err := sql.Open(dbType, username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&parseTime=True&loc=Asia%2FKolkata")
-	//conn, err := sql.Open(dbType, "root:123456@tcp(127.0.0.1:3306)/testdb")
+	url := username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&parseTime=True&loc=Asia%2FKolkata"
+	conn, err := sql.Open(dbType, url)
 
 	if err != nil {
 		return nil, err
