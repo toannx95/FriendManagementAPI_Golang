c9fbc58f6a80f96d342f447a62139f19d537ac6f
toannx95 <toannx95@gmail.com>
2020-04-20 10:25:59 +0700

    update

--- src/config/InitDB.go
+++ src/config/InitDB.go
@@ -2,25 +2,32 @@ package config
 
 import (
 	"database/sql"
-	"fmt"
+	//"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
-	"os"
+	//"github.com/joho/godotenv"
+	//"os"
 )
 
 func OpenDB() (*sql.DB, error) {
-	env := godotenv.Load("./src/.env")
-	if env != nil {
-		fmt.Print(env)
-	}
+	//env := godotenv.Load("./src/.env")
+	//if env != nil {
+	//	fmt.Print(env)
+	//}
+	//
+	//username := os.Getenv("DB_USER")
+	//password := os.Getenv("DB_PASSWORD")
+	//dbName := os.Getenv("DB_NAME")
+	//dbHost := os.Getenv("DB_HOST")
+	//dbPort := os.Getenv("DB_PORT")
 
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+	username := "root"
+	password := "123456"
+	dbtype := "mysql"
+	dbName := "testdb"
+	dbHost := "localhost"
+	dbPort := "3306"
 
-	conn, err := sql.Open("mysql", username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&parseTime=True&loc=Asia%2FKolkata")
+	conn, err := sql.Open(dbtype, username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&parseTime=True&loc=Asia%2FKolkata")
 	//conn, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/testdb")
 
 	if err != nil {
