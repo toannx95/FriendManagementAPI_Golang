3a709aa91cafaa648d9fa9b9cbebae9545cd1262
toannx95 <toannx95@gmail.com>
2020-04-20 11:22:29 +0700

    update

--- src/config/InitDB.go
+++ src/config/InitDB.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+
 	//"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	//"github.com/joho/godotenv"
@@ -28,10 +30,18 @@ func OpenDB() (*sql.DB, error) {
 	dbPort := "3306"
 
 	conn, err := sql.Open(dbType, username + ":" + password + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8&parseTime=True&loc=Asia%2FKolkata")
-	//conn, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/testdb")
+	//conn, err := sql.Open(dbType, "root:123456@tcp(127.0.0.1:3306)/testdb")
 
 	if err != nil {
 		return nil, err
+	} else {
+		fmt.Println("DB connected!")
 	}
+
+	err = conn.Ping()
+	if err != nil {
+		return nil, err
+	}
+
 	return conn, nil
 }
