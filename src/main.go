09a0656d58a3376703f3ce09e4faaac7b62527ae
S3CORP\toan.nguyenx <toannx95@gmail.com>
2020-04-27 17:45:31 +0700

    update .env file and docs

--- src/main.go
+++ src/main.go
@@ -13,7 +13,6 @@ import (
 // @contact.email soberkoder@swagger.io
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8081
 // @BasePath /
 func main() {
 	db, _ := config.OpenDB()
